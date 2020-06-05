67cd029163ee148e9d392afe7e45970bbc907e02
jtempest <54986678+jtempest@users.noreply.github.com>
2020-06-05 10:49:16 +0100

    builder - 06

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -2,7 +2,10 @@ extern crate proc_macro;
 
 use proc_macro2::{Ident, Span, TokenStream};
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Type};
+use syn::{
+    parse_macro_input, Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, GenericArgument,
+    PathArguments, Type,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
@@ -17,15 +20,20 @@ fn generate_builder(input: DeriveInput) -> Result<TokenStream, syn::Error> {
     let builder_name = Ident::new(&format!("{}Builder", struct_name), Span::call_site());
     let fields = FieldsInfo::new(&input)?;
 
-    let init_exprs = fields.for_each(|name, _| {
+    let init_exprs = fields.for_each(|field| {
+        let name = &field.name;
         quote! { #name: None }
     });
 
-    let builder_fields = fields.for_each(|name, ty| {
+    let builder_fields = fields.for_each(|field| {
+        let name = &field.name;
+        let ty = &field.ty;
         quote! { #name: Option<#ty> }
     });
 
-    let builder_methods = fields.for_each(|name, ty| {
+    let builder_methods = fields.for_each(|field| {
+        let name = &field.name;
+        let ty = &field.ty;
         quote! {
             fn #name(&mut self, #name: #ty) -> &mut Self {
                 self.#name = Some(#name);
@@ -34,11 +42,16 @@ fn generate_builder(input: DeriveInput) -> Result<TokenStream, syn::Error> {
         }
     });
 
-    let builder_results = fields.for_each(|name, _| {
-        quote! {
-            #name: self.#name.clone().ok_or_else(|| Box::<std::error::Error>::from(
-                concat!("Required field '", stringify!(#name), "' has not been set.")
-            ))?
+    let builder_results = fields.for_each(|field| {
+        let name = &field.name;
+        if field.is_optional {
+            quote! { #name: self.#name.clone() }
+        } else {
+            quote! {
+                #name: self.#name.clone().ok_or_else(|| Box::<std::error::Error>::from(
+                    concat!("Required field '", stringify!(#name), "' has not been set.")
+                ))?
+            }
         }
     });
 
@@ -67,9 +80,13 @@ fn generate_builder(input: DeriveInput) -> Result<TokenStream, syn::Error> {
     })
 }
 
-struct FieldsInfo<'a> {
-    names_and_types: Vec<(Ident, &'a Type)>,
+struct FieldInfo<'a> {
+    name: Ident,
+    ty: &'a Type,
+    is_optional: bool,
 }
+
+struct FieldsInfo<'a>(Vec<FieldInfo<'a>>);
 
 impl FieldsInfo<'_> {
     fn new(input: &DeriveInput) -> Result<FieldsInfo, syn::Error> {
@@ -78,22 +95,27 @@ impl FieldsInfo<'_> {
             "Cannot generate builder for this type",
         ))?;
 
-        let names_and_types: Option<Vec<_>> = named_fields
-            .map(|f| Some((f.ident.clone()?, &f.ty)))
+        let fields: Option<Vec<_>> = named_fields
+            .map(|f| {
+                let name = f.ident.clone()?;
+                let (ty, is_optional) = read_field_type(&f.ty);
+                Some(FieldInfo {
+                    name,
+                    ty,
+                    is_optional,
+                })
+            })
             .collect();
-        let names_and_types = names_and_types.ok_or(syn::Error::new(
+        let fields = fields.ok_or(syn::Error::new(
             Span::call_site(),
             "Cannot generate builder for unnamed field",
         ))?;
 
-        Ok(FieldsInfo { names_and_types })
+        Ok(FieldsInfo(fields))
     }
 
-    fn for_each<F: std::ops::Fn(&Ident, &Type) -> TokenStream>(&self, func: F) -> Vec<TokenStream> {
-        self.names_and_types
-            .iter()
-            .map(|(name, ty)| func(name, ty))
-            .collect()
+    fn for_each<F: std::ops::Fn(&FieldInfo) -> TokenStream>(&self, func: F) -> Vec<TokenStream> {
+        self.0.iter().map(|f| func(f)).collect()
     }
 }
 
@@ -105,4 +127,24 @@ fn named_fields_iter(input: &DeriveInput) -> Option<impl Iterator<Item = &Field>
         }) => Some(named.iter()),
         _ => None,
     }
+}
+
+// (ty, is_optional): if optional, returns the type contained by the Option.
+fn read_field_type(ty: &Type) -> (&Type, bool) {
+    if let Type::Path(path) = ty {
+        if path.qself.is_none() {
+            let segments = &path.path.segments;
+            if segments.len() > 0 {
+                let seg0 = &segments[0];
+                if seg0.ident.to_string() == "Option" {
+                    if let PathArguments::AngleBracketed(args) = &seg0.arguments {
+                        if let Some(GenericArgument::Type(optional_type)) = args.args.first() {
+                            return (optional_type, true);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    (ty, false)
 }
